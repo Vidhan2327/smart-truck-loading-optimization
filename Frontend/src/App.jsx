e9661811eed21e345238e3ae9ba570fbdcf14884
Vidhan Singh Vaish <vidhansinghvaish@gmail.com>
2025-12-19 17:57:49 +0530

    task-1: project setup and base architecture

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Smart Truck Loading Optimization System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
