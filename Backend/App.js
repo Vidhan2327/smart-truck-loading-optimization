fce9ad6d0080b3cef88bd24cc77f7492f96074e4
Vidhan2327 <156836400+Vidhan2327@users.noreply.github.com>
2025-12-22 11:44:20 +0530

    Fix variable naming and update CORS origin

--- Backend/App.js
+++ Backend/App.js
@@ -4,27 +4,26 @@ const cors = require("cors");
 
 const authRoutes = require("./Routes/AuthRoutes");
 const shipmentRoutes = require("./Routes/ShipmentRoutes");
-const TruckRoutes = require("./Routes/TruckRoutes");
-
-
-
+const truckRoutes = require("./Routes/TruckRoutes");
 
 const app = express();
+
+app.set("trust proxy", 1);
 
 app.use(express.json());
 app.use(cookieParser());
 
-
 app.use(
   cors({
-    origin: true,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
 
+app.get("/health", (_, res) => res.send("OK"));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/shipments", shipmentRoutes);
-app.use("/api/trucks", TruckRoutes);
-
+app.use("/api/trucks", truckRoutes);
 
 module.exports = app;
